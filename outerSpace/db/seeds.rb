7764e7db46a376efac17eea108149b2451c9f6a1
jayad25 <dammalapati.jaya@gmail.com>
2019-04-30 13:20:14 -0700

    create setup file

--- outerSpace/db/seeds.rb
+++ outerSpace/db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ship.destroy_all
+System.destroy_all
+system1 = System.create!(name:"milkiway1",radius:10)
+system2 = System.create!(name:"kepler",radius:11)
+system3 = System.create!(name:"rock",radius:100)
+
+ship1 = Ship.create!(name:"Enterprice",velocity:1000,system_id:system1.id)
+ship1 = Ship.create!(name:"Houston",velocity:2000,system_id:system1.id)
+ship1 = Ship.create!(name:"Austin",velocity:4000,system_id:system2.id)
+ship1 = Ship.create!(name:"Dallas",velocity:5000,system_id:system3.id)
+ship1 = Ship.create!(name:"San Antonio",velocity:8000,system_id:system3.id)
